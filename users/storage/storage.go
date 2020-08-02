d58d73f59cdb784b251df56167f0d25e74abff24
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-08-02 16:58:10 +0200

    API test will now be an integration one

--- users/storage/storage.go
+++ users/storage/storage.go
@@ -17,6 +17,9 @@ func New(addr string, password string) *Storage {
 	return nil
 }
 
+// AddUser adds a user with the given parameters, returning its ID in the case
+// of success or an error otherwise.
+// If an user with the given email already exists it returns users.UserAlreadyExistsErr
 func (s *Storage) AddUser(
 	ctx context.Context,
 	email users.Email,
